54ed2acad2bb09c56008a374ffaf3a745cb26a7e
Cristianalbertus <10969sosho@Cristianalbertuss-MacBook-Air.local>
2026-09-02 22:21:16 +0700

    fix(member-home): full-bleed member card on mobile

--- resources/js/Pages/Member/Home.jsx
+++ resources/js/Pages/Member/Home.jsx
@@ -336,7 +336,7 @@ export default function Home({ member, banners = [], vendor_ranking = [] }) {
                     )}
                 </header>
 
-                <section className="flex justify-center px-2">
+                <section className="-mx-4 flex justify-center sm:mx-0">
                     <MemberCard member={member} />
                 </section>
 
