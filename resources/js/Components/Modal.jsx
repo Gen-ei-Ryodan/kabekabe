cd5fa5806790400c7e49dbdbf7cbb5b0c2932654
Cristianalbertus <10969sosho@Cristianalbertuss-MacBook-Air.local>
2026-09-03 09:42:57 +0700

    fix(ui): center modal on mobile

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -54,7 +54,7 @@ export default function Modal({
                     leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
                     <DialogPanel
-                        className={`mb-6 transform overflow-hidden rounded-lg bg-white shadow-xl transition-all sm:mx-auto sm:w-full ${maxWidthClass}`}
+                        className={`mx-auto w-full transform overflow-hidden rounded-lg bg-white shadow-xl transition-all ${maxWidthClass}`}
                     >
                         {children}
                     </DialogPanel>
